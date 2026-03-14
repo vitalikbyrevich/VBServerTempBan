3e0d286c5bdc3ba90d53b7e87d69b07fe7143c9d
Vladi <vitalik_byrevich@mail.ru>
2026-03-15 00:33:57 +0300

    u

---
+++ VBServerTempBan/GlobalUsings.cs
@@ -0,0 +1,11 @@
+﻿global using System.Reflection;
+global using BepInEx;
+global using BepInEx.Configuration;
+global using HarmonyLib;
+global using UnityEngine;
+global using System;
+global using System.Collections.Generic;
+global using System.Globalization;
+global using System.IO;
+global using System.Linq;
+global using BepInEx.Logging;
